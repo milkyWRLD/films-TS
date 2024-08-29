ca4d05c46d8d2b7616d0ecce0b6bb732fc0e62e3
Artyom Sidelnikov <you@example.com>
2024-08-29 15:36:50 +0300

    add files

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Stars from './Stars'
+
+function App() {
+	return (
+		<>
+			<div style={{ border: 'solid' }}>
+				<Stars count={0} />
+				<Stars count={1} />
+				<Stars count={2} />
+				<Stars count={3} />
+				<Stars count={4} />
+				<Stars count={5} />
+				<Stars count={6} />
+			</div>
+		</>
+	)
+}
+
+export default App
